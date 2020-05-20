e1edd3158a652b170b74e7bbc93b0890fc4ef722
scarter13 <scarter13@gmail.com>
2020-05-19 20:04:48 -0400

    no punctuatuion, lower case, spaces instead of hyphens

--- word_frequency.py
+++ word_frequency.py
@@ -4,10 +4,35 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
+import string
+punctuation = string.punctuation
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    opened_file = open(file)
+    text = opened_file.read()
+    print(text)
+        #replace hyphens
+    no_hyphen = text.replace("-"," ")
+        #remove punctuation
+    no_punctuation = ""
+    for char in no_hyphen:
+        if char not in punctuation:
+            no_punctuation = no_punctuation + char
+        #make everything lowercase
+    lower_case_text = no_punctuation.lower()
+    print(lower_case_text)
+        #split into words
+    words = lower_case_text.split(" ")
+    print(words)
+
+    
+
+
+
+    #remove the stop words
+    #count the frequency of the remaing words (see ex 6 for sort function)
+    #output as a cord list, count and graph of ***
 
 
 if __name__ == "__main__":
